927899c5b1c9cfbe4a58eea2d805dce71a10b660
Гусев Владислав Владимирович <v.gusev@etpgpb.local>
2023-02-17 17:28:34 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LayoutComponent } from './layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from 'src/environments/environment';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { ItemState } from './state/items.state';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { NgxsRootModule } from '@ngxs/store/src/modules/ngxs-root.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Select, Store, Actions, ActionCompletion, ofAction, ofActionDispatched, ofActionSuccessful, ofActionCompleted } from '@ngxs/store';
+import { Observable, takeUntil, Subject } from 'rxjs';
 import { Item } from '../models/item';
 import { AddItemAction, RemoveItemAction } from '../state/items.actions';
 import { ItemState } from '../state/items.state';
@@ -10,15 +10,46 @@ import { ItemState } from '../state/items.state';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
   @Select(ItemState.getAllItems) allItems$: Observable<Item[]> | undefined;
 
   loadingItems: boolean[] = [];
   item = '';
 
-  constructor(private store: Store) {};
+  private ngUnscubscribe = new Subject<void>();
 
-  ngOnInit(): void {};
+  constructor(private store: Store, private actions$: Actions) {};
+
+  ngOnInit(): void {
+    //получаем 2 action'а: при диспатче и при завершении
+    // this.actions$.pipe(ofAction(AddItemAction, RemoveItemAction), takeUntil(this.ngUnscubscribe))
+
+    //в этом случае получим action ТОЛЬКО в момент диспатча
+    // this.actions$.pipe(ofActionDispatched(AddItemAction, RemoveItemAction), takeUntil(this.ngUnscubscribe))
+
+    //тут только в момент завершения (completed)
+    // this.actions$.pipe(ofActionSuccessful(AddItemAction, RemoveItemAction), takeUntil(this.ngUnscubscribe))
+
+    //в payload получаем объект action'а и result object, в котором говорится в каком статусе выполнился action
+    this.actions$.pipe(ofActionCompleted(AddItemAction, RemoveItemAction), takeUntil(this.ngUnscubscribe))
+    .subscribe((data: ActionCompletion) => {
+      if (data.result.successful) {
+        if (data.action instanceof RemoveItemAction) {
+          //inform user about the removal
+        } else if (data.action instanceof AddItemAction) {
+          // inform user about the item addition
+        } else if (data.result.error) {
+          //inform the user about an error
+        }
+      }
+      this.loadingItems.pop();
+    });
+  };
+
+  ngOnDestroy(): void {
+    this.ngUnscubscribe.next();
+    this.ngUnscubscribe.complete();
+  }
 
   addItem(itemName: string): void {
     this.loadingItems.push(true);
@@ -31,4 +62,5 @@ export class LayoutComponent implements OnInit {
   removeItem(id: number): void {
     this.store.dispatch(new RemoveItemAction(id));
   }
+
 }
